3df865aa63374aaa82c0d5d6fbb887e15d84e8ff
Rameesha <rameeshakhan75@gmail.com>
2025-07-12 15:21:48 +0500

    tailwind added

---
+++ Frontend/src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+const Navbar = () => {
+    return (
+        <>
+            <div className="navbar-logo">
+                <img src="/logo.png" className="navbar-logo-img"></img>
+            </div>
+        </>
+
+    )
+}
+
+export default Navbar
